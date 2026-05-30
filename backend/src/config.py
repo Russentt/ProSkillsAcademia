066c238a63978df13212c329aff6a47ddc17ef3a
Lukita-duoc <luc.ortizr@duocuc.cl>
2026-05-30 16:57:42 -0400

    2 commit

---
+++ backend/src/config.py
@@ -0,0 +1,11 @@
+class DevelopmentConfig():
+    DEBUG = True
+    PORT = 8080
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = ''
+    
+config = {
+    'development': DevelopmentConfig
+}
