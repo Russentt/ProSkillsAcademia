c94ee964f8b28fd2b056ab545b42a1defd01be5e
Russentt <ignacio.battistoni.mestre@gmail.com>
2026-05-30 20:17:54 -0400

    Arreglar boton vinculado a inicio de sesion

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
